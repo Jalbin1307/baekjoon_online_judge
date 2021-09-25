7475b9dfa3eed5d6d88fa3a90723bdef4073f252
Jalbin1307 <az13077@gmail.com>
2021-09-25 21:07:56 +0900

    docs : modify 1157.py

--- step_07/1157.py
+++ step_07/1157.py
@@ -1,22 +1,16 @@
-S = input()
-S = S.upper()
-max,chk = 0, -1
-c =''
-dic = {chr(i) : int(0) for i in range(65,91)}
+S = input().upper()
+dic = {chr(i) : 0 for i in range(65,91)}
 
-for i in range(len(S)):
-    dic[S[i]] += 1
+for key in dic.keys():
+    dic[key] = S.count(key)
 
-for i in dic.keys():
-    if dic[i] >= max:
-        if dic[i] == max:
-            chk = 1
-        else:
-            chk = 0
-        max = dic[i]
-        c = i
+val = list(dic.values())
+val.sort(reverse=True)
 
-if chk == 1:
+if val[0]==val[1]:
     print("?")
-elif chk == 0:
-    print(c)
+else:
+    for key in dic.keys():
+        if dic[key] == val[0]:
+            print(key)
+            break
